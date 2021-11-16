cf9c5ddefbd0a60324e31fc84df6df6d861c15b3
rss <opqlns@pm.me>
2021-11-15 18:43:35 -0600

    initial_roughdraft_commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var count = 3
+countElement = document.querySelector("#likes-1")
+
+function increase1(){
+    count++;
+    countElement.innerText = count + " like(s)";
+}
+
+var count2 = 12
+countElement2 = document.querySelector("#likes-2")
+
+function increase2(){
+    count2++;
+    countElement2.innerText = count2 + " like(s)";
+}
+
+var count3 = 9
+countElement3 = document.querySelector("#likes-3")
+
+function increase3(){
+    count3++;
+    countElement3.innerText = count3 + " like(s)";
+}
